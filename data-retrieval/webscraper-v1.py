7fe30fd31e3eda74a83035e5eed295b014b71646
evanengelhardt <eaengelh@gmail.com>
2018-05-16 21:01:42 -0500

    Added ability to retrieve win pct, ppg, and oppg when looking at a games match up

--- data-retrieval/webscraper-v1.py
+++ data-retrieval/webscraper-v1.py
@@ -11,20 +11,40 @@ page_soup = soup(page_html, "html.parser")
 
 filename = "practice-nba-games.csv"
 f = open(filename, "w")
-headers = "winning-team, winning-points, losing-team, losing-points\n"
+headers = "visitor-win-pct, visitor-ppg, visitor-oppg, home-win-pct, home-ppg, home-oppg, winner\n"
 f.write(headers)
 
 containers = page_soup.findAll("div", {"class": "game_summary"})
 
+team_stat_tables = page_soup.findAll("table", {"class": "stats_table"})
+
+
+def find_stats(team_url):
+    for team_stat_table in team_stat_tables:
+        team_stat_containers = team_stat_table.findAll("tr", {"class": "full_table"})
+
+        for team_stat_container in team_stat_containers:
+            if team_url == team_stat_container.findAll("th", {"data-stat": "team_name"})[0].a["href"]:
+                team_wl = team_stat_container.findAll("td", {"data-stat": "win_loss_pct"})[0].text
+                team_pts_per_game = team_stat_container.findAll("td", {"data-stat": "pts_per_g"})[0].text
+                team_opp_pts_per_game = team_stat_container.findAll("td", {"data-stat": "opp_pts_per_g"})[0].text
+
+                f.write(str(team_wl) + "," + str(team_pts_per_game) + "," + str(team_opp_pts_per_game) + ",")
+                return
+
+
 for container in containers:
-    loser_container = container.findAll("tr", {"class": "loser"})
-    loser_name = loser_container[0].td.a.text
-    loser_score = loser_container[0].findAll("td", {"class": "right"})[0].text
+    visitor_container = container.findAll("tr")[0]
+    find_stats(visitor_container.td.a["href"])
 
-    winner_container = container.findAll("tr", {"class": "winner"})
-    winner_name = winner_container[0].td.a.text
-    winner_score = winner_container[0].findAll("td", {"class": "right"})[0].text
+    home_container = container.findAll("tr")[1]
+    find_stats(home_container.td.a["href"])
 
-    f.write(winner_name + "," + winner_score + "," + loser_name + "," + loser_score + "\n")
+    winner_container = container.findAll("tr", {"class" : "winner"})[0]
+
+    if winner_container == visitor_container:
+        f.write("v\n")
+    else:
+        f.write("h\n")
 
 f.close()
