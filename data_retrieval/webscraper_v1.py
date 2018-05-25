d7649beba40a2f9970111474a7d34b36638513fe
evanengelhardt <eaengelh@gmail.com>
2018-05-25 15:08:37 -0500

    Refactored web scraper script and begun to integrate mysql connector in order to eventually move data from csv to a db

--- data_retrieval/webscraper_v1.py
+++ data_retrieval/webscraper_v1.py
@@ -1,22 +1,33 @@
 from data_retrieval.url_processing import UrlProcessing
-from data_retrieval.url_setup import UrlSetUp
 
 filename = "practice-nba-games.csv"
+base_url = 'https://www.basketball-reference.com/boxscores/'
+days_of_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-# All of the 15-16 and 16-17 seasons
-startMonth = 10
-startDay = 30
-startYear = 2012
 
-endMonth = 4
-endDay = 14
-endYear = 2017
+def get_data(start_month, start_day, start_year, end_month, end_day, end_year):
+    while start_month != end_month or start_day != end_day or start_year != end_year:
+        url_data = update_url(start_month, start_day, start_year)
+        new_url = url_data[0]
+        print(new_url)
+        UrlProcessing(new_url, filename)
 
-while startMonth != endMonth or startDay != endDay or startYear != endYear:
-    new_url = UrlSetUp(startMonth, startDay, startYear)
-    UrlProcessing(new_url.newUrl, filename)
-    startMonth = new_url.month
-    startDay = new_url.day + 1
-    startYear = new_url.year
+        start_month = url_data[1]
+        start_day = url_data[2] + 1
+        start_year = url_data[3]
 
-    print(new_url.newUrl)
+
+def update_url(month, day, year):
+    if day > days_of_month[month - 1]:
+        month += 1
+        day = 1
+    if month > 12:
+        month = 1
+        year += 1
+    if month == 4 and day == 15:
+        month = 10
+        print("Starting Season: " + str(year))
+
+    updated_url = base_url + '?month=' + str(month) + '&day=' + str(day) + '&year=' + str(year)
+
+    return updated_url, month, day, year
